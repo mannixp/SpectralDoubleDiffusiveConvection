766c186614a2a38ff7c2d3595d7985097290ee40
mannixp <pm4615@ic.ac.uk>
2023-12-18 20:18:06 +0100

    Added and Validated nonlinear tests

--- Transforms.py
+++ Transforms.py
@@ -3,7 +3,7 @@ import numpy as np
 import copy
 from   scipy.fftpack import dct,dst
 
-L  = np.pi/2.;
+L  = np.pi;
 
 def grid(N):
 
@@ -20,7 +20,8 @@ def sinusoid_to_IDCT(f_hat):
     """
 
     f_hat_scaled      = copy.deepcopy(f_hat)
-    f_hat_scaled[1:] *=.5;
+    f_hat_scaled[...,1:] *=.5;
+    
 
     return f_hat_scaled;
 
@@ -31,9 +32,9 @@ def DCT_to_sinusoid(f_hat_scaled):
     """
 
     f_hat     = copy.deepcopy(f_hat_scaled)
-    N         = f_hat.shape[0] 
+    N         = f_hat.shape[-1] 
     f_hat    *= 1/N
-    f_hat[0] *= 0.5
+    f_hat[...,0] *= 0.5
 
     return f_hat
 
@@ -44,11 +45,11 @@ def sinusoid_to_IDST(g_hat):
     g_hat_scaled = copy.deepcopy(g_hat)
     g_hat_scaled *=0.5; 
     
-    # N = g_hat.shape[0]
-    # start = g_hat_scaled[0:N-1];
-    # shift = g_hat_scaled[1:N  ];
-    # np.copyto(start,shift);
-    # g_hat_scaled[-1] = 0.; # Drop Nyquist mode
+    N = g_hat.shape[-1]
+    start = g_hat_scaled[...,0:N-1];
+    shift = g_hat_scaled[...,1:N  ];
+    np.copyto(start,shift);
+    g_hat_scaled[...,-1] = 0.; # Drop Nyquist mode
     
     return g_hat_scaled;
 
@@ -58,12 +59,12 @@ def DST_to_sinusoid(g_hat_scaled):
     Scale DST output to sinusoid amplitudes
     """
     g_hat = copy.deepcopy(g_hat_scaled)
-    N     = g_hat.shape[0]
+    N     = g_hat.shape[-1]
 
-    # start = g_hat[0:N-1];
-    # shift = g_hat[1:N  ];
-    # np.copyto(shift,start);
-    # g_hat[0] = 0
+    start = g_hat[...,0:N-1];
+    shift = g_hat[...,1:N  ];
+    np.copyto(shift,start);
+    g_hat[...,0] = 0
     g_hat   *= 1/N
 
     return g_hat
@@ -79,7 +80,7 @@ def DST(g,n=None,axis=-1):
     if n == None:
         g_hat = DST_to_sinusoid(g_hat_scaled)
     else:
-        g_hat = DST_to_sinusoid(g_hat_scaled)[0:n]
+        g_hat = DST_to_sinusoid(g_hat_scaled)[...,0:n]
 
     return g_hat;
 
@@ -108,7 +109,7 @@ def DCT(f,n=None,axis=-1):
     if n == None:
         f_hat = DCT_to_sinusoid(f_hat_scaled)
     else:
-        f_hat = DCT_to_sinusoid(f_hat_scaled)[0:n]
+        f_hat = DCT_to_sinusoid(f_hat_scaled)[...,0:n]
 
     return f_hat;
 
@@ -139,13 +140,6 @@ def Test_Cosine_Transform(k,N):
     print('~~~~ Cosine coefficient space to grid space~~~~~~')
 
     f     = IDCT(f_hat_in)
-    
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    plt.plot(x,f_in,'r:')
-    plt.plot(x,f,'bo')
-    plt.show()
-
     f_hat = DCT(f)
     
     for a,b in zip(np.round(f_hat,12),f_hat_in):
@@ -154,7 +148,13 @@ def Test_Cosine_Transform(k,N):
     print('~~~~ Cosine grid space to coefficient space~~~~~~')
 
     f_hat = DCT(f_in)
-    f     = IDCT(f_hat) 
+    f     = IDCT(f_hat)
+
+    # import matplotlib.pyplot as plt
+    # fig = plt.figure()
+    # plt.plot(x,f_in,'r:')
+    # plt.plot(x,f,'bo')
+    # plt.show() 
     
     for a,b in zip(np.round(f,12),np.round(f_in,12)):
         assert a == b
@@ -167,7 +167,7 @@ def Test_Sine_Transform(k,N):
     g_hat_in[k] = 1
 
     x     = grid(N)
-    g_in  = np.sin((k+1)*x)
+    g_in  = np.sin(k*x)
 
     # %%
     print('~~~~ Sine coefficient space to grid space~~~~~~')
@@ -247,7 +247,7 @@ def Test_Sine_Transform_deal(k,N):
     # print('g_hat = ',g_hat   )
 
     print('~~~~ Sine grid space to coefficient space~~~~~~')
-
+    
     g_hat=  DST(g_in,n=N)
     g    = IDST(g_hat,n=N)
 
@@ -260,21 +260,133 @@ def Test_Sine_Transform_deal(k,N):
 
     return None;
 
-# Test for 2D data
+# Test Nonlinear
+def Test_Sine_Transform_NL(N):
+
+    g_hat_in    = np.zeros(N)
+    x           = grid(N)
+
+    # sin(x) by cos(x)
+    g_hat_in[2] = 0.5
+    g_in  = np.sin(1*x)*np.cos(1*x)
+
+    # sin(x) by cos(2x)
+    g_hat_in[1] += -0.5
+    g_hat_in[3] +=  0.5
+    g_in  += np.sin(1*x)*np.cos(2*x)
+
+    # sin(2x) by cos(x)
+    g_hat_in[1] += 0.5
+    g_hat_in[3] += 0.5
+    g_in  += np.sin(2*x)*np.cos(1*x)
+
+    # %%
+    print('~~~~ Sine coefficient space to grid space~~~~~~')
+    g    = IDST(g_hat_in,n=N)#(3*N)//2)
+    g_hat=  DST(g,n=N)
+
+    for a,b in zip(np.round(g_hat,12),g_hat_in):
+        assert a == b
+
+    for a,b in zip(np.round(g,12),np.round(g_in,12)):
+        assert a == b    
+
+    # print('g_hat = ',g_hat_in)
+    # print('g     = ',g       )
+    # print('g_in  = ',g_in    )
+    # print('g_hat = ',g_hat   )
+
+    print('~~~~ Sine grid space to coefficient space~~~~~~')
+    
+    g_hat=  DST(g_in,n=N)
+    g    = IDST(g_hat,n=N)
+
+    for a,b in zip(np.round(g,12),np.round(g_in,12)):
+        assert a == b
+
+    for a,b in zip(np.round(g_hat,12),np.round(g_hat_in,12)):
+        assert a == b
+
+    # print('g     = ',g_in )
+    # print('g_hat = ',g_hat)
+    # print('g_hat_in = ',g_hat_in)
+    # print('g     = ',g    )
+
+    return None;
+
+def Test_Cosine_Transform_NL(N):
+
+    x         = grid(N)
+    f_hat_in  = np.zeros(N)
+
+    # cos(1x) by cos(2x)
+    f_hat_in[1] = 1
+    f_hat_in[3] = 1
+    f_in = 2*np.cos(x)*np.cos(2*x)
+
+    # cos(x) by cos(x)
+    f_hat_in[0] += 1
+    f_hat_in[2] += 1
+    f_in += 2*np.cos(x)*np.cos(x)
+
+    #sin(1x) by sin(2x)
+    f_hat_in[1] += 0.5
+    f_hat_in[3] += -.5
+    f_in += np.sin(x)*np.sin(2*x)
+
+    # sin(x) by sin(x)
+    f_hat_in[0] += 0.5
+    f_hat_in[2] += -.5
+    f_in += np.sin(x)*np.sin(x)
+
+    print('~~~~ Cosine coefficient space to grid space~~~~~~')
+
+    f     = IDCT(f_hat_in,n=N)#(3*N)//2) 
+    f_hat =  DCT(f,n=N)
+
+    # print('f_hat_in = ',f_hat_in)
+    # print('f        = ',f       )
+    # print('f_in     = ',f_in    )
+    # print('f_hat    = ',f_hat   )
+
+    for a,b in zip(np.round(f_hat,12),f_hat_in):
+        assert a == b
+    
+    for a,b in zip(np.round(f,12),np.round(f_in,12)):
+        assert a == b
+
+    print('~~~~ Cosine grid space to coefficient space~~~~~~')
+
+    f_hat =  DCT(f_in ,n=N)
+    f     = IDCT(f_hat,n=N) 
+    
+    for a,b in zip(np.round(f,12),np.round(f_in,12)):
+        assert a == b
+    
+    for a,b in zip(np.round(f_hat,12),np.round(f_hat_in,12)):
+        assert a == b
+
+    # print('f_in     = ',f_in    )
+    # print('f_hat    = ',f_hat   )    
+    # print('f_hat_in = ',f_hat_in)
+    # print('f        = ',f       )
+    
+    return None;
+
 
 if __name__ == "__main__":
 
     # 1D Data
-    N = 10;
-    k = 1;
-    Test_Cosine_Transform(k,N);
+    N = 2**8;
 
+    Test_Cosine_Transform_NL(N)
+    Test_Sine_Transform_NL(N)
 
-    # for k in range(3):
-    #     Test_Cosine_Transform(k,N);
-    #     Test_Sine_Transform(k,N);
+    for k in range(3):
+        Test_Cosine_Transform(k,N);
+        Test_Sine_Transform(k+1,N);
 
-    # #1D Data + Aliasing
-    # for k in range(3):
-    #     Test_Cosine_Transform_deal(k,N);
-    #     Test_Sine_Transform_deal(k,N);
+    #1D Data + Aliasing
+    for k in range(3):
+        Test_Cosine_Transform_deal(k,N);
+        Test_Sine_Transform_deal(k+1,N);
